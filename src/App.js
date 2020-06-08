db140b98368d6bd036e572a07109fb42ce03402a
christiano39 <carneson39@gmail.com>
2020-06-08 13:20:34 -0600

    seperated score display and score update buttons into components

--- src/App.js
+++ src/App.js
@@ -1,59 +1,60 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, { useState } from "react";
-import BottomRow from "./BottomRow";
+import React, { useState } from "react"
+import BottomRow from "./BottomRow"
 import "./App.css";
+//eslint-disable-next-line
+import { render } from "@testing-library/react"
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, setHomeScore] = useState(0)
   const [awayScore, setAwayScore] = useState(0)
 
-  const homeTD = e => {
-    setHomeScore(homeScore + 7)
+  const touchdown = (isHome, e) => {
+    isHome ? setHomeScore(homeScore + 7) : setAwayScore(awayScore + 7)
   }
-  const homeFieldGoal = e => {
-    setHomeScore(homeScore + 3)
-  }
-
-  const awayTD = e => {
-    setAwayScore(awayScore + 7)
-  }
-  const awayFieldGoal = e => {
-    setAwayScore(awayScore + 3)
+  const fieldGoal = (isHome, e) => {
+    isHome ? setHomeScore(homeScore + 3) : setAwayScore(awayScore + 3)
   }
 
   return (
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
-
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-            <div className="home__score">{homeScore}</div>
-          </div>
+          <TeamName isHome name="Lions" homeScore={homeScore} awayScore={awayScore} />
           <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{awayScore}</div>
-          </div>
+          <TeamName isHome={false} name="Tigers" homeScore={homeScore} awayScore={awayScore} />
         </div>
         <BottomRow />
       </section>
       <section className="buttons">
-        <div className="homeButtons">
-
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={homeTD}>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal" onClick={homeFieldGoal}>Home Field Goal</button>
-        </div>
-        <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={awayTD}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick={awayFieldGoal}>Away Field Goal</button>
-        </div>
+        <Buttons isHome touchdown={touchdown} fieldGoal={fieldGoal} />
+        <Buttons isHome={false} touchdown={touchdown} fieldGoal={fieldGoal} />
       </section>
     </div>
-  );
+  )
+}
+
+function TeamName(props){
+  const { isHome, name, homeScore, awayScore } = props
+  const prefix = isHome ? 'home' : 'away'
+  return (
+    <div className={prefix}>
+      <h2 className={prefix + "__name"}>{name}</h2>
+  <div className={prefix + "__score"}>{isHome ? homeScore : awayScore}</div>
+    </div>
+  )
+}
+
+function Buttons(props){
+  const { isHome, touchdown, fieldGoal } = props
+  const prefix = isHome ? 'home' : 'away'
+  return (
+    <div className={prefix + 'Buttons'}>
+      <button className={prefix + "Buttons__touchdown"} onClick={() => touchdown(props.isHome)}>{prefix.toUpperCase()} Touchdown</button>
+      <button className={prefix + "Buttons__fieldGoal"} onClick={() => fieldGoal(props.isHome)}>{prefix.toUpperCase()} Field Goal</button>
+    </div>
+  )
 }
 
 export default App;
